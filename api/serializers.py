a7a703a8b1d1d57a2b774299a709cf3a8bd0b964
AS-Yousef <a.s.sultan@hotmail.com>
2018-12-17 19:45:19 +0300

    task_15 fatma&Anfal

--- api/serializers.py
+++ api/serializers.py
@@ -9,3 +9,14 @@ class RestaurantListSerializer(serializers.ModelSerializer):
         	'opening_time',
         	'closing_time',
         	]
+
+class RestaurantDetailsSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = Restaurant
+		fields = ['id','owner','name','description','opening_time','closing_time',]
+
+
+class RestaurantCreateUpdateSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = Restaurant
+		fields = ['name','description','opening_time','closing_time',]
